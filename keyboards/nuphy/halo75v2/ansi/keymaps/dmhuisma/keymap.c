112b898c730e520ea9f7bc58f90e3d0f6682912a
Darcy Huisman <darcyhuisman@gmail.com>
2026-01-02 20:04:59 -0500

    colemak-dh layer

--- keyboards/nuphy/halo75v2/ansi/keymaps/dmhuisma/keymap.c
+++ keyboards/nuphy/halo75v2/ansi/keymaps/dmhuisma/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC,	KC_BSLS,                KC_END,
 	KC_CAPS,	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	KC_ENT, 	                        KC_PGUP,
 	KC_LSFT,				KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,                KC_UP,	    KC_PGDN,
-	KC_LCTL,	KC_LWIN,	KC_LALT,										KC_SPC, 							            KC_RALT,	MO(3),	                KC_LEFT,   	KC_DOWN,	KC_RIGHT),
+	KC_LCTL,	KC_LWIN,	KC_RALT,										KC_SPC, 							            KC_RCTL,	MO(3),	                KC_LEFT,   	KC_DOWN,	KC_RIGHT),
 // layer Standard Fn
 [1] = LAYOUT_ansi_84(
 	_______, 	KC_BRID,  	KC_BRIU,  	SOCDON, 	SOCDOFF,  	SOCDTOG,  	_______,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	WIN_PRTA,	TOG_POWER_ON_ANIMATION,	_______,
@@ -23,14 +23,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	TOG_CAPS_IND,	SLEEP_TIMEOUT_DEC,   	SLEEP_TIMEOUT_SHOW,   	SLEEP_TIMEOUT_INC,  	_______,   	_______,   	DEBOUNCE_RELEASE_DEC,	DEBOUNCE_RELEASE_SHOW,   	DEBOUNCE_RELEASE_INC,   	_______,  	_______,	_______, 	_______,                            _______,
 	_______,				_______,   	_______,   	RGB_TEST,  	_______,    _______,   	_______,	MO(4), 		RGB_SPD,	RGB_SPI,	_______,	_______,	            RGB_VAI,    _______,
 	_______,	_______,	_______,										_______, 							            KC_SCRL,	MO(3),					RGB_MOD,    RGB_VAD,	RGB_HUI),
-// layer home row mod
+// colemak-dh
 [2] = LAYOUT_ansi_84(
 	KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	KC_F12,   	KC_INS,     KC_DEL,
 	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,                KC_HOME,
-	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC,	KC_BSLS,                KC_END,
-	KC_CAPS,LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,   	KC_H,RSFT_T(KC_J),RCTL_T(KC_K),LALT_T(KC_L),RGUI_T(KC_SCLN),KC_QUOT, 	KC_ENT, 	                        KC_PGUP,
-	KC_LSFT,				KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,                KC_UP,	    KC_PGDN,
-	KC_LCTL,	KC_LWIN,	KC_LALT,										KC_SPC, 							            KC_RALT,	MO(3),	                KC_LEFT,   	KC_DOWN,	KC_RIGHT),
+	KC_TAB, 	KC_Q,   	KC_W,   	KC_F,  		KC_P,   	KC_B,   	KC_J,   	KC_L,   	KC_U,   	KC_Y,  		KC_SCLN,   	KC_LBRC,	KC_RBRC,	KC_BSLS,                KC_END,
+	KC_CAPS,	KC_A,   	KC_R,   	KC_S,  		KC_T,   	KC_G,   	KC_M,   	KC_N,   	KC_E,   	KC_I,  		KC_O,		KC_QUOT, 	KC_ENT, 	                        KC_PGUP,
+	KC_LSFT,				KC_X,   	KC_C,   	KC_D,  		KC_V,   	KC_Z,   	KC_K,   	KC_H,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,                KC_UP,	    KC_PGDN,
+	KC_LCTL,	KC_LWIN,	KC_RALT,										KC_SPC, 							            KC_RCTL,	MO(3),	                KC_LEFT,   	KC_DOWN,	KC_RIGHT),
+// layer home row mod
+// [2] = LAYOUT_ansi_84(
+// 	KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	KC_F12,   	KC_INS,     KC_DEL,
+// 	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,                KC_HOME,
+// 	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC,	KC_BSLS,                KC_END,
+// 	KC_CAPS,LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,   	KC_H,RSFT_T(KC_J),RCTL_T(KC_K),LALT_T(KC_L),RGUI_T(KC_SCLN),KC_QUOT, 	KC_ENT, 	                        KC_PGUP,
+// 	KC_LSFT,				KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,                KC_UP,	    KC_PGDN,
+// 	KC_LCTL,	KC_LWIN,	KC_LALT,										KC_SPC, 							            KC_RALT,	MO(3),	                KC_LEFT,   	KC_DOWN,	KC_RIGHT),
 // layer home row mod Fn
 [3] = LAYOUT_ansi_84(
 	_______, 	KC_BRID,  	KC_BRIU,  	SOCDON, 	SOCDOFF,  	SOCDTOG,  	_______,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	WIN_PRTA,	TOG_POWER_ON_ANIMATION,	_______,
